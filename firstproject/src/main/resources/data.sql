dd8654165e35d3b226d9ccaf99b2a3038453590e
강수민 <kangsm423@gmail.com>
2024-07-06 15:31:54 +0900

    11장 : HTTP와 REST 컨트롤러

--- firstproject/src/main/resources/data.sql
+++ firstproject/src/main/resources/data.sql
@@ -4,3 +4,7 @@ INSERT INTO article(title, content) VALUES ('다다다다', '3333');
 INSERT INTO member(id, email, password) VALUES (1, 'mail@cloudstudying.kr', '1111');
 INSERT INTO member(id, email, password) VALUES (2, 'sehongpark@cloudstudying.kr', '2222');
 INSERT INTO member(id, email, password) VALUES (3, 'email@cloudstudying.kr', '3333');
+-- 11장 셀프체크 데이터 추가
+INSERT INTO coffee(name, price) VALUES ('아메리카노', '4500');
+INSERT INTO coffee(name, price) VALUES ('라떼', '5000');
+INSERT INTO coffee(name, price) VALUES ('카페 모카', '5500');
