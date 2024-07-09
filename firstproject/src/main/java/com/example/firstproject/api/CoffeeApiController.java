1c0ebc467372284dc307e57a208c49526293e0cf
강수민 <kangsm423@gmail.com>
2024-07-09 18:45:21 +0900

    12장 : 서비스 계층과 트랜잭션

--- firstproject/src/main/java/com/example/firstproject/api/CoffeeApiController.java
+++ firstproject/src/main/java/com/example/firstproject/api/CoffeeApiController.java
@@ -2,7 +2,7 @@ package com.example.firstproject.api;
 
 import com.example.firstproject.dto.CoffeeDto;
 import com.example.firstproject.entity.Coffee;
-import com.example.firstproject.repository.CoffeeRepository;
+import com.example.firstproject.service.CoffeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,63 +15,50 @@ import java.util.List;
 @Slf4j
 public class CoffeeApiController {
     @Autowired
-    private CoffeeRepository coffeeRepository;
+    private CoffeeService coffeeService;
 
     // GET
     @GetMapping("/api/coffees")
-    public List<Coffee> index() {
-        return coffeeRepository.findAll();
+    public ResponseEntity<List<Coffee>> index() {
+        List<Coffee> coffeeList = coffeeService.index();
+        return coffeeList != null ?
+                ResponseEntity.status(HttpStatus.OK).body(coffeeList) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
+
     @GetMapping("/api/coffees/{id}")
-    public Coffee show(@PathVariable Long id) {
-        return coffeeRepository.findById(id).orElse(null);
+    public ResponseEntity<Coffee> show(@PathVariable Long id) {
+        Coffee coffee = coffeeService.show(id);
+        return coffee != null ?
+                ResponseEntity.status(HttpStatus.OK).body(coffee) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
     // POST
     @PostMapping("/api/coffees")
-    public Coffee create(@RequestBody CoffeeDto dto) {
-        Coffee coffee = dto.toEntity();
-        return coffeeRepository.save(coffee);
+    public ResponseEntity<Coffee> create(@RequestBody CoffeeDto dto) {
+        Coffee created = coffeeService.create(dto);
+        return created != null ?
+                ResponseEntity.status(HttpStatus.OK).body(created) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
     // PATCH
     @PatchMapping("/api/coffees/{id}")
     public ResponseEntity<Coffee> update(@PathVariable Long id, @RequestBody CoffeeDto dto) {
-        // 1. DTO -> 엔티티 변환하기
-        Coffee coffee = dto.toEntity();
-        log.info("id: {}, Coffee: {}", id, coffee);
-
-        // 2. 타깃 조회하기
-        Coffee target = coffeeRepository.findById(id).orElse(null);
-
-        // 3. 잘못된 요청 처리하기
-        if (target == null || id != coffee.getId()) {
-            // 400, 잘못된 요청 응답!
-            log.info("잘못된 요청! id: {}, coffee: {}", id, coffee);
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-
-        // 4. 업데이트 및 정상 응답(200)하기
-        target.patch(coffee);
-        Coffee updated = coffeeRepository.save(target);
-        return ResponseEntity.status(HttpStatus.OK).body(updated);
+        Coffee updated = coffeeService.update(id, dto);
+        return updated != null ?
+                ResponseEntity.status(HttpStatus.OK).body(updated) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
     // DELETE
     @DeleteMapping("/api/coffees/{id}")
     public ResponseEntity<Coffee> create(@PathVariable Long id) {
-        // 1. 대상 찾기
-        Coffee target = coffeeRepository.findById(id).orElse(null);
-
-        // 2. 잘못된 요청 처리하기
-        if (target == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-
-        // 3. 대상 삭제하기
-        coffeeRepository.delete(target);
-//        return ResponseEntity.status(HttpStatus.OK).body(null);
-        return ResponseEntity.status(HttpStatus.OK).build();
+        Coffee deleted = coffeeService.delete(id);
+        return deleted != null ?
+                ResponseEntity.status(HttpStatus.OK).body(deleted) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
 }
